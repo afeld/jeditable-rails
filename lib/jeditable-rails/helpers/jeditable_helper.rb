224053dbdd5b7d3b9c044c7504357978062fd3f5
Aidan Feldman <aidan.feldman@gmail.com>
2011-02-01 05:34:07 -0500

    added editable field helper

--- lib/jeditable-rails/helpers/jeditable_helper.rb
+++ lib/jeditable-rails/helpers/jeditable_helper.rb
@@ -1,5 +1,17 @@
 module JeditableHelper
-  def foo
-    "bar"
+  def editable_field(object, property, options={})
+    name = "#{object.class.to_s.underscore}[#{property}]"
+    value = object.send property
+    %{
+      <span class="editable" data-name="#{name}">#{value}</span>
+      <script type="text/javascript">
+        $(function(){
+          $(".editable[data-name='#{name}']").editable("#{url_for(object)}", {
+            method: 'PUT',
+            name: "#{name}"
+          });
+        });
+      </script>
+    }.html_safe
   end
 end
