1c3ebb3c38bce065d02cc0f39f31de852fcb3960
Aidan Feldman <aidan.feldman@gmail.com>
2011-02-02 00:15:21 -0500

    unescaping HTML for input field, and allow options to be passed to editable_field

--- lib/jeditable-rails/helpers/jeditable_helper.rb
+++ lib/jeditable-rails/helpers/jeditable_helper.rb
@@ -2,14 +2,23 @@ module JeditableHelper
   def editable_field(object, property, options={})
     name = "#{object.class.to_s.underscore}[#{property}]"
     value = object.send property
+    update_url = options.delete(:update_url) || url_for(object)
+    args = {:method => 'PUT', :name => name}.merge(options)
     %{
       <span class="editable" data-name="#{name}">#{value}</span>
       <script type="text/javascript">
-        $(function(){
-          $(".editable[data-name='#{name}']").editable("#{url_for(object)}", {
-            method: 'PUT',
-            name: "#{name}"
-          });
+        $(function(){          
+          var args = {data: function(value, settings) {
+            // Unescape HTML
+            var retval = value
+              .replace(/&amp;/gi, '&')
+              .replace(/&gt;/gi, '>')
+              .replace(/&lt;/gi, '<')
+              .replace(/&quot;/gi, "\\\"");
+            return retval;
+          }};
+          $.extend(args, #{args.to_json});
+          $(".editable[data-name='#{name}']").editable("#{update_url}", args);
         });
       </script>
     }.html_safe
